7082b4ac73985e7179b55a31ead2c0b312e2ef22
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-09-15 23:04:01 +0300

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className='header'>
+            <h3><img src = './images/Trollface.png' className='troll'/> Meme Generator</h3>
+            <span>React Course-Project 3</span>
+          
+        </div>
+    )
+}
